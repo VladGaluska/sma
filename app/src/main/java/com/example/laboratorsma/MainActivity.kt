eb9888fbef6688c1f27b8707e0518885b07bc5b4
Vlad Galuska <vlad.galuska@hibyte.ro>
2020-10-07 23:18:08 +0300

    Added button, input field and output field

--- app/src/main/java/com/example/laboratorsma/MainActivity.kt
+++ app/src/main/java/com/example/laboratorsma/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.laboratorsma
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var messageKeeper: MessageKeeper = MessageKeeper("Hello world!");
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.textView).apply {
+            text = messageKeeper.message;
+        }
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        messageKeeper.message = editText.text.toString()
+        findViewById<TextView>(R.id.textView).apply {
+            text = messageKeeper.message
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/laboratorsma/MessageKeeper.kt
@@ -0,0 +1,3 @@
+package com.example.laboratorsma
+
+data class MessageKeeper (var message: String)
